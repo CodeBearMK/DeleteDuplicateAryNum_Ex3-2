a3446436ae282a2cc986c2efe71e98bb14e5f293
cat32tw <mike1993xd@gmail.com>
2022-02-06 14:32:57 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DeleteDuplicateAryNum_Ex3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] AryNum = { 23, 12, 34, 12, 45, 12, 23 };
+            Console.Write("1. 陣列的初值：");
+            for (int i = 0; i < AryNum.Length; i++)
+            {
+                Console.Write("{0}, ", AryNum[i]);
+            }
+            Console.WriteLine();
+
+            Console.Write("2. 刪除重複的值後：");
+            Array.Sort(AryNum);
+            AryNum = AryNum.Distinct().ToArray();
+            for (int j = 0; j < AryNum.Length; j++)
+            {
+                Console.Write("{0}, ", AryNum[j]);
+            }
+            Console.Read();
+        }
+    }
+}
